2a1138f2ecb93c483d8a8f033f88168496736aa0
Hayoung Lee <emitlighthy@naver.com>
2024-09-02 01:59:06 +0900

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -6,8 +6,8 @@ def calculate_d_day(target_date):
     delta = target - today
     return delta.days
 
-target_date = "2024-12-25"  # 목표 날짜 설정
+target_date = "2024-10-19"  # 목표 날짜 설정
 d_day = calculate_d_day(target_date)
 
-with open("README.md", "w") as f:
+with open(".github/profile/README.md", "w") as f:
     f.write(f"# D-Day: {d_day} days remaining\n")
