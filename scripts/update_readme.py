54a122be334aefb6c4c95a7ea6373b4f8ed29f18
Hayoung Lee <emitlighthy@naver.com>
2024-09-02 02:15:32 +0900

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -10,7 +10,9 @@ def calculate_d_day(target_date):
 target_date = "2024-10-19"  # 목표 날짜 설정
 d_day = calculate_d_day(target_date)
 
-# 정확한 경로로 profile 디렉토리의 README.md 파일 업데이트
-readme_path = os.path.join(".github", "profile", "README.md")
+# 루트 디렉토리 기준으로 절대 경로 설정
+root_dir = os.getenv('GITHUB_WORKSPACE', '')  # GitHub Actions의 루트 디렉토리 가져오기
+readme_path = os.path.join(root_dir, ".github", "profile", "README.md")
+
 with open(readme_path, "w") as f:
-    f.write(f"# D-Day: {d_day} days remaining\n")
+    f.write(f"# Engineer-Information-Processing\n\n## D-Day\n현재 목표일까지 남은 날짜는 {d_day}일입니다.")
